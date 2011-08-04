eb3b76f05a02106475a3e9bb3094dcc4060908d0
Michael S. Klishin <michael@novemberain.com>
2011-08-04 18:33:52 +0400

    Outline monger.collection API

--- src/monger/collection.clj
+++ src/monger/collection.clj
@@ -6,6 +6,18 @@
 ;; API
 ;;
 
+;; monger.collection/insert
+;; monger.collection/find
+;; monger.collection/group
+
+;; monger.collection/count
 (defn ^long count
   [^DB db, ^String collection]
   (.count (.getCollection db collection)))
+
+;; monger.collection/update
+;; monger.collection/update-multi
+;; monger.collection/remove
+
+;; monger.collection/ensure-index
+;; monger.collection/drop-index
