f822e2411f31b3a7009852ea00dc0b6383b64ea9
Michael S. Klishin <michael@novemberain.com>
2011-08-16 16:55:56 +0400

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.novemberain/monger "0.1.0-SNAPSHOT"
+(defproject com.novemberain/monger "0.1.0"
   :description "Monger is an experimental idiomatic Clojure wrapper around MongoDB Java driver"
   :license { :name "Eclipse Public License" }
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]
