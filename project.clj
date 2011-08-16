7361497f1816b8f5f378b3750065ad8757bccf0e
Michael S. Klishin <michael@novemberain.com>
2011-08-16 16:55:36 +0400

    Add license information to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject com.novemberain/monger "0.1.0-SNAPSHOT"
   :description "Monger is an experimental idiomatic Clojure wrapper around MongoDB Java driver"
+  :license { :name "Eclipse Public License" }
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  [org.mongodb/mongo-java-driver "2.6.3"]])
