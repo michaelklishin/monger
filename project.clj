c9eeb48dea7a1eb779258b94d6c69acfbf87cd86
Michael S. Klishin <michael@novemberain.com>
2011-08-27 14:06:31 +0400

    Clojure 1.3.0-beta2 is out

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.novemberain/monger "0.2.0-SNAPSHOT"
   :description "Monger is an experimental idiomatic Clojure wrapper around MongoDB Java driver"
   :license { :name "Eclipse Public License" }
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.3.0-beta2"]
                  [org.mongodb/mongo-java-driver "2.6.3"]])
