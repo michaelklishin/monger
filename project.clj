7f2e3bc46e33d65bb57799be3332411a998552f4
Michael S. Klishin <michael@novemberain.com>
2011-09-01 17:14:34 +0400

    Bump mongo driver dependency

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Monger is an experimental idiomatic Clojure wrapper around MongoDB Java driver"
   :license { :name "Eclipse Public License" }
   :dependencies [[org.clojure/clojure "1.3.0-beta2"]
-                 [org.mongodb/mongo-java-driver "2.6.3"]]
+                 [org.mongodb/mongo-java-driver "2.6.5"]]
   :warn-on-reflection true)
