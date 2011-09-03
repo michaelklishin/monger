c29748e46c27bf7b2f95dad87ae43c348226ab20
Michael S. Klishin <michael@novemberain.com>
2011-09-04 00:49:47 +0400

    Add monger.json that extends clojure.data.json.Write-JSON protocol to handle org.bson.types.ObjectId

---
+++ src/monger/json.clj
@@ -0,0 +1,8 @@
+(ns monger.json
+  (:import (org.bson.types ObjectId))
+  (:require [clojure.data.json :as json]))
+
+(extend-protocol json/Write-JSON
+  ObjectId
+  (write-json [^ObjectId object out escape-unicode?]
+    (json/write-json (.toString object) out escape-unicode?)))
