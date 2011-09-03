1609f0fab28f1266331237cb9cdc48fd395ec806
Michael S. Klishin <michael@novemberain.com>
2011-09-03 17:54:11 +0400

    Upgrade to Clojure 1.3.0-beta3

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.novemberain/monger "0.2.0-SNAPSHOT"
   :description "Monger is an experimental idiomatic Clojure wrapper around MongoDB Java driver"
   :license { :name "Eclipse Public License" }
-  :dependencies [[org.clojure/clojure "1.3.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.3.0-beta3"]
                  [org.mongodb/mongo-java-driver "2.6.5"]]
   :warn-on-reflection true)
