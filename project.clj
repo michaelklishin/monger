668373c894c9d7b448927b1510e678df2b185bef
Michael S. Klishin <michael@novemberain.com>
2011-09-09 00:37:53 +0400

    tools.cli should be a development dependency, it is not needed at runtime

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "Monger is an experimental idiomatic Clojure wrapper around MongoDB Java driver"
   :license { :name "Eclipse Public License" }
   :dependencies [[org.clojure/clojure "1.3.0-beta3"]
-                 [org.mongodb/mongo-java-driver "2.6.5"]
-                 [org.clojure/tools.cli     "0.1.0"]
-                 ]
+                 [org.mongodb/mongo-java-driver "2.6.5"]]
+  :dev-dependencies [[org.clojure/tools.cli     "0.1.0"]]
   :warn-on-reflection true)
