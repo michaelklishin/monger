27358efbbc4d2def1e89836b780858450a043001
Michael S. Klishin <michael@novemberain.com>
2011-09-14 14:32:17 +0400

    Use Clojure 1.3.0-RC0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.novemberain/monger "0.7.0-SNAPSHOT"
   :description "Monger is an experimental idiomatic Clojure wrapper around MongoDB Java driver"
   :license { :name "Eclipse Public License" }
-  :dependencies [[org.clojure/clojure "1.3.0-beta3"]
+  :dependencies [[org.clojure/clojure "1.3.0-RC0"]
                  [org.mongodb/mongo-java-driver "2.6.5"]]
   :warn-on-reflection true)
