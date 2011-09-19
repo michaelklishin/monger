c5850bd59fee57dba2ef23a0dc169f33158bad5d
Michael S. Klishin <michael@novemberain.com>
2011-09-19 12:31:29 +0400

    Get rid of monger.util/with-ns
    It was an awful hack, now replaced by a less awful one.

--- src/monger/util.clj
+++ src/monger/util.clj
@@ -29,13 +29,6 @@
   []
   (ObjectId.))
 
-(defmacro with-ns
-  "Evaluates body in another namespace.  ns is either a namespace object or a symbol.
-   This makes it possible to define functions in namespaces other than the current one."
-  [ns & body]
-  `(binding [*ns* (the-ns ~ns)]
-     ~@(map (fn [form] `(eval '~form)) body)))
-
 (defprotocol GetDocumentId
   (get-id  [input] "Returns document id"))
 
