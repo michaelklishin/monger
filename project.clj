e2a16795578bc14729f7d623712800036016eb58
Michael S. Klishin <michael@novemberain.com>
2011-10-09 14:00:16 +0400

    0.9.0-SNAPSHOT, trying to investigate some clojars.org dependency resolution issues

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.novemberain/monger "0.8.0-SNAPSHOT"
+(defproject com.novemberain/monger "0.9.0-SNAPSHOT"
   :description "Monger is an experimental idiomatic Clojure wrapper around MongoDB Java driver"
   :license { :name "Eclipse Public License" }
   :dependencies [[org.clojure/clojure "1.3.0"]
