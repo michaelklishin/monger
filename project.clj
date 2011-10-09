48bb69ddb268cb5a97a0d69d76cfe548a306d312
Michael S. Klishin <michael@novemberain.com>
2011-10-10 02:26:03 +0400

    Add Validateur as a [soft] dependency, it greatly complements Monger

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject com.novemberain/monger "0.9.0-SNAPSHOT"
+(defproject com.novemberain/monger "0.10.0-SNAPSHOT"
   :description "Monger is an experimental idiomatic Clojure wrapper around MongoDB Java driver"
   :license { :name "Eclipse Public License" }
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.mongodb/mongo-java-driver "2.6.5"]]
+  :dependencies [[org.clojure/clojure           "1.3.0"]
+                 [org.mongodb/mongo-java-driver "2.6.5"]
+                 [com.novemberain/validateur    "1.0.0-SNAPSHOT"]]
   :warn-on-reflection true)
