e2b6c6e21bd616b13cfdc4a0c9911e717470b15a
Michael S. Klishin <michael@novemberain.com>
2011-10-18 17:43:11 +0400

    Update clojure.data.json to 0.1.2

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure           "1.3.0"]
                  [org.mongodb/mongo-java-driver "2.6.5"]
                  [com.novemberain/validateur    "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[org.clojure/data.json "0.1.1"]
-                     [clj-time              "0.3.1"]]
+  :dev-dependencies [[org.clojure/data.json "0.1.2"]
+                     [clj-time              "0.3.1" :exclusions [org.clojure/clojure]]]
   :dev-resources-path "test/resources"
   :warn-on-reflection true)
