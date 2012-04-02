59b32e119c1053f2c46328a66de6cce2fb38eee6
Michael S. Klishin <michael@novemberain.com>
2012-04-02 12:13:05 +0400

    Test against Clojure 1.4.0-beta6

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  :archive "https://groups.google.com/group/clojure-monger",
                  :post "clojure-monger@googlegroups.com"}
   :profiles {:1.4 {:resource-paths ["test/resources"],
-                   :dependencies [[org.clojure/clojure "1.4.0-beta5"]]},
+                   :dependencies [[org.clojure/clojure "1.4.0-beta6"]]},
              :dev {:resource-paths ["test/resources"],
                    :dependencies  [[org.mongodb/mongo-java-driver "2.7.3"]
                                    [com.novemberain/validateur "1.0.0"]
