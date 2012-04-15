379fb73795428e498e3f40ecafdb2b2fb335a3b1
Michael S. Klishin <michael@novemberain.com>
2012-04-16 00:44:08 +0400

    Guard one more test that relies on authentication/ordering

--- test/monger/test/core_test.clj
+++ test/monger/test/core_test.clj
@@ -23,17 +23,18 @@
   (let [connection (monger.core/connect { :host "127.0.0.1" })]
     (is (instance? com.mongodb.Mongo connection))))
 
-(deftest connect-to-mongo-via-uri-without-credentials
-  (let [connection (monger.core/connect-via-uri! "mongodb://127.0.0.1/monger-test4")]
-    (is (= (-> connection .getAddress ^InetAddress (.sameHost "127.0.0.1")))))
-  ;; reconnect using regular host
-  (helper/connect!))
+(when-not (System/getenv "CI")
+  (deftest connect-to-mongo-via-uri-without-credentials
+    (let [connection (monger.core/connect-via-uri! "mongodb://127.0.0.1/monger-test4")]
+      (is (= (-> connection .getAddress ^InetAddress (.sameHost "127.0.0.1")))))
+    ;; reconnect using regular host
+    (helper/connect!))
 
-(deftest connect-to-mongo-via-uri-with-valid-credentials
-  (let [connection (monger.core/connect-via-uri! "mongodb://clojurewerkz/monger!:monger!@127.0.0.1/monger-test4")]
-    (is (= (-> connection .getAddress ^InetAddress (.sameHost "127.0.0.1")))))
-  ;; reconnect using regular host
-  (helper/connect!))
+  (deftest connect-to-mongo-via-uri-with-valid-credentials
+    (let [connection (monger.core/connect-via-uri! "mongodb://clojurewerkz/monger!:monger!@127.0.0.1/monger-test4")]
+      (is (= (-> connection .getAddress ^InetAddress (.sameHost "127.0.0.1")))))
+    ;; reconnect using regular host
+    (helper/connect!)))
 
 (if-let [uri (System/getenv "MONGOHQ_URL")]
   (deftest ^{:external true} connect-to-mongo-via-uri-with-valid-credentials
