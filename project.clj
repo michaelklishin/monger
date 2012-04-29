ea1e611ccffb3b459a55823c6a0d6a793c6b0e7f
Michael S. Klishin <michael@novemberain.com>
2012-04-29 18:24:11 +0400

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.novemberain/monger "1.0.0-beta5"
+(defproject com.novemberain/monger "1.0.0-SNAPSHOT"
   :description "Monger is a Clojure MongoDB client for a more civilized age: friendly, flexible and with batteries included"
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"}
