43349f65ae88fb9c42349f3598314c305c3f3a96
Michael S. Klishin <michael@novemberain.com>
2012-06-27 19:02:11 +0400

    Add an alternative Ring session store that uses Clojure reader serialization
    This way libraries like Friend, that use namespaced keywords (::identity) and other
    Clojure-specific data structures will work well with Monger.
    
    Current store will strip off namespace information from namespaced keywords
    because clojure.core/name work that way. For example: (name ::identity).
    
    Reported by Julio Barros.

--- src/clojure/monger/ring/session_store.clj
+++ src/clojure/monger/ring/session_store.clj
@@ -18,6 +18,49 @@
 ;; API
 ;;
 
+;; this session store stores Clojure data structures using Clojure reader. It will correctly store every
+;; data structure Clojure reader can serialize and read but won't make the data useful to applications
+;; in other languages.
+
+(defrecord ClojureReaderBasedMongoDBSessionStore [^String collection-name])
+
+(extend-protocol ringstore/SessionStore
+  ClojureReaderBasedMongoDBSessionStore
+
+  (read-session [store key]
+    #_ (if-let [m (and key
+                       (mc/find-one-as-map (.collection-name store) {:_id key}))]
+         m
+         {})
+    (if key
+      (if-let [m (mc/find-one-as-map (.collection-name store) {:_id key})]
+        (read-string (:value m))
+        {})
+      {}))
+
+  (write-session [store key data]
+    (let [date  (Date.)
+          key   (or key (str (UUID/randomUUID)))
+          value (binding [*print-dup* true]
+                  (pr-str (assoc data :date date :_id key)))]
+      (mc/save (.collection-name store) {:_id key :date date :value value})
+      key))
+
+  (delete-session [store key]
+    (mc/remove-by-id (.collection-name store) key)
+    nil))
+
+
+(defn session-store
+  ([]
+     (ClojureReaderBasedMongoDBSessionStore. default-session-store-collection))
+  ([^String s]
+     (ClojureReaderBasedMongoDBSessionStore. s)))
+
+
+;; this session store won't store namespaced keywords correctly but stores results in a way
+;; that applications in other languages can read. DO NOT use it with Friend.
+
 (defrecord MongoDBSessionStore [^String collection-name])
 
 (extend-protocol ringstore/SessionStore
