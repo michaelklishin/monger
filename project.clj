ea493eee1d77c4c13fb0e749b55277a92bd47c96
Michael Klishin <michael@defprotocol.org>
2013-07-06 14:48:11 +0400

    Back to dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.novemberain/monger "1.6.0"
+(defproject com.novemberain/monger "1.7.0-SNAPSHOT"
   :description "Monger is a Clojure MongoDB client for a more civilized age: friendly, flexible and with batteries included"
   :url "http://clojuremongodb.info"
   :min-lein-version "2.0.0"
