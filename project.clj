2e64a24cc75e639f2192c428b93db5c0c2e9be73
Michael Klishin <michael@clojurewerkz.org>
2014-03-27 18:13:10 +0400

    Depend on Clojure 1.6

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://clojuremongodb.info"
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"}
-  :dependencies [[org.clojure/clojure           "1.5.1"]
+  :dependencies [[org.clojure/clojure           "1.6.0"]
                  [org.mongodb/mongo-java-driver "2.12.0-rc0"]
                  [clojurewerkz/support          "0.20.0"]]
   :test-selectors {:default     (fn [m]
