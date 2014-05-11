d7965adb86d1d294043967f1bcb1db6202f9796e
Michael Klishin <michael@clojurewerkz.org>
2014-05-11 13:37:18 -0400

    Remove tests that are now duplicates

--- test/monger/test/ring/clojure_session_store_test.clj
+++ test/monger/test/ring/clojure_session_store_test.clj
@@ -22,22 +22,7 @@
     (is (= {} (read-session store "a-missing-key-1228277")))))
 
 
-(deftest test-reading-a-session-that-does-not-exist-given-db
-  (let [store (session-store db "sessions")]
-    (is (= {} (read-session store "a-missing-key-1228277")))))
-
-
 (deftest test-reading-a-session-that-does-exist
-  (let [store (session-store db "sessions")
-        sk    (write-session store nil {:library "Monger"})
-        m     (read-session store sk)]
-    (is sk)
-    (is (and (:_id m)))
-    (is (= (dissoc m :_id)
-           {:library "Monger"}))))
-
-
-(deftest test-reading-a-session-that-does-exist-given-db
   (let [store (session-store db "sessions")
         sk    (write-session store nil {:library "Monger"})
         m     (read-session store sk)]
@@ -58,28 +43,8 @@
     (is (= (dissoc m :_id)
            {:library "Ring"}))))
 
-
-(deftest test-updating-a-session-given-db
-  (let [store (session-store db "sessions")
-        sk1   (write-session store nil {:library "Monger"})
-        sk2   (write-session store sk1 {:library "Ring"})
-        m     (read-session store sk2)]
-    (is (and sk1 sk2))
-    (is (and (:_id m)))
-    (is (= sk1 sk2))
-    (is (= (dissoc m :_id)
-           {:library "Ring"}))))
-
-
 (deftest test-deleting-a-session
   (let [store (session-store db "sessions")
         sk    (write-session store nil {:library "Monger"})]
     (is (nil? (delete-session store sk)))
     (is (= {} (read-session store sk)))))
-
-
-(deftest test-deleting-a-session
-  (let [store (session-store db "sessions")
-        sk    (write-session store nil {:library "Monger"})]
-    (is (nil? (delete-session store sk)))
-    (is (= {} (read-session store sk))))))
