3fda07999d02da1056e63ea99e676d920386352d
Michael Klishin <michael@clojurewerkz.org>
2014-05-11 13:39:37 -0400

    An typo

--- test/monger/test/ring/clojure_session_store_test.clj
+++ test/monger/test/ring/clojure_session_store_test.clj
@@ -9,42 +9,42 @@
 (let [conn (mg/connect)
       db   (mg/get-db conn "monger-test")]
   (defn purge-sessions
-  [f]
-  (mc/remove db "sessions")
-  (f)
-  (mc/remove db "sessions"))
+    [f]
+    (mc/remove db "sessions")
+    (f)
+    (mc/remove db "sessions"))
 
-(use-fixtures :each purge-sessions)
-
-
-(deftest test-reading-a-session-that-does-not-exist
-  (let [store (session-store db "sessions")]
-    (is (= {} (read-session store "a-missing-key-1228277")))))
+  (use-fixtures :each purge-sessions)
 
 
-(deftest test-reading-a-session-that-does-exist
-  (let [store (session-store db "sessions")
-        sk    (write-session store nil {:library "Monger"})
-        m     (read-session store sk)]
-    (is sk)
-    (is (and (:_id m)))
-    (is (= (dissoc m :_id)
-           {:library "Monger"}))))
+  (deftest test-reading-a-session-that-does-not-exist
+    (let [store (session-store db "sessions")]
+      (is (= {} (read-session store "a-missing-key-1228277")))))
 
 
-(deftest test-updating-a-session
-  (let [store (session-store db "sessions")
-        sk1   (write-session store nil {:library "Monger"})
-        sk2   (write-session store sk1 {:library "Ring"})
-        m     (read-session store sk2)]
-    (is (and sk1 sk2))
-    (is (and (:_id m)))
-    (is (= sk1 sk2))
-    (is (= (dissoc m :_id)
-           {:library "Ring"}))))
+  (deftest test-reading-a-session-that-does-exist
+    (let [store (session-store db "sessions")
+          sk    (write-session store nil {:library "Monger"})
+          m     (read-session store sk)]
+      (is sk)
+      (is (and (:_id m)))
+      (is (= (dissoc m :_id)
+             {:library "Monger"}))))
 
-(deftest test-deleting-a-session
-  (let [store (session-store db "sessions")
-        sk    (write-session store nil {:library "Monger"})]
-    (is (nil? (delete-session store sk)))
-    (is (= {} (read-session store sk)))))
+
+  (deftest test-updating-a-session
+    (let [store (session-store db "sessions")
+          sk1   (write-session store nil {:library "Monger"})
+          sk2   (write-session store sk1 {:library "Ring"})
+          m     (read-session store sk2)]
+      (is (and sk1 sk2))
+      (is (and (:_id m)))
+      (is (= sk1 sk2))
+      (is (= (dissoc m :_id)
+             {:library "Ring"}))))
+
+  (deftest test-deleting-a-session
+    (let [store (session-store db "sessions")
+          sk    (write-session store nil {:library "Monger"})]
+      (is (nil? (delete-session store sk)))
+      (is (= {} (read-session store sk))))))
