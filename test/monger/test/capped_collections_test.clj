f02a2b0afc2610bc5b41ea890c46da0061184d55
Michael Klishin <michael@clojurewerkz.org>
2014-05-11 13:12:30 -0400

    Capped collection tests now pass

--- test/monger/test/capped_collections_test.clj
+++ test/monger/test/capped_collections_test.clj
@@ -1,33 +1,25 @@
 (ns monger.test.capped-collections-test
-  (:require [monger core util]
+  (:require [monger.core :as mg]
             [monger.collection  :as mc]
-            [monger.result      :as mres]
-            [monger.test.helper :as helper]
             [clojure.test :refer :all]
-            [monger.operators :refer :all]
-            [monger.test.fixtures :refer :all]))
-
-(helper/connect!)
+            [monger.operators :refer :all]))
 
 (defn- megabytes
   [^long n]
   (* n 1024 1024))
 
-
-;;
-;; Tests
-;;
-
-(deftest test-inserting-into-capped-collection
-  (let [n     1000
-        cname "cached"
-        _     (mc/drop cname)
-        coll  (mc/create cname {:capped true :size (-> 16 megabytes) :max n})]
-    (is (= cname (.getName coll)))
-    (mc/insert-batch cname (for [i (range 0 (+ n 100))] {:i i}))
-    (is (= n (mc/count cname)))
-    ;; older elements get replaced by newer ones
-    (is (not (mc/any? cname {:i 1})))
-    (is (not (mc/any? cname {:i 5})))
-    (is (not (mc/any? cname {:i 9})))
-    (is (mc/any? cname {:i (+ n 80)}))))
+(let [conn (mg/connect)
+      db   (mg/get-db conn "monger-test")]
+  (deftest test-inserting-into-capped-collection
+    (let [n     1000
+          cname "cached"
+          _     (mc/drop db cname)
+          coll  (mc/create db cname {:capped true :size (-> 16 megabytes) :max n})]
+      (is (= cname (.getName coll)))
+      (mc/insert-batch db cname (for [i (range 0 (+ n 100))] {:i i}))
+      (is (= n (mc/count db cname)))
+      ;; older elements get replaced by newer ones
+      (is (not (mc/any? db cname {:i 1})))
+      (is (not (mc/any? db cname {:i 5})))
+      (is (not (mc/any? db cname {:i 9})))
+      (is (mc/any? db cname {:i (+ n 80)})))))
