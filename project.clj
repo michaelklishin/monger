0495d74a34784c2b1544e8bd30018a25719b25ed
Michael Klishin <michael@clojurewerkz.org>
2014-12-07 16:55:39 +0300

    Java driver 2.12.4

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure           "1.6.0"]
-                 [org.mongodb/mongo-java-driver "2.12.1"]
+                 [org.mongodb/mongo-java-driver "2.12.4"]
                  [clojurewerkz/support          "1.1.0"]]
   :test-selectors {:default     (fn [m]
                                   (and (not (:performance m))
