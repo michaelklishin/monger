49cb195e5a0df4351db4b3a26c3d64d75904899e
Michael Klishin <michael@clojurewerkz.org>
2015-05-11 00:04:31 +0300

    Adapt before_script.sh to createUser in 3.0 GA

--- bin/ci/before_script.sh
+++ bin/ci/before_script.sh
@@ -5,7 +5,7 @@ sleep 5
 
 # MongoDB Java driver won't run authentication twice on the same DB instance,
 # so we need to use multiple DBs.
-mongo --eval 'db.createUser("clojurewerkz/monger", "monger")' monger-test
-mongo --eval 'db.createUser("clojurewerkz/monger", "monger")' monger-test2
-mongo --eval 'db.createUser("clojurewerkz/monger", "monger")' monger-test3
-mongo --eval 'db.createUser("clojurewerkz/monger", "monger")' monger-test4
+mongo --eval 'db.createUser({"user": "clojurewerkz/monger", "pwd": "monger", roles: ["dbAdmin"]})' monger-test
+mongo --eval 'db.createUser({"user": "clojurewerkz/monger", "pwd": "monger", roles: ["dbAdmin"]})' monger-test2
+mongo --eval 'db.createUser({"user": "clojurewerkz/monger", "pwd": "monger", roles: ["dbAdmin"]})' monger-test3
+mongo --eval 'db.createUser({"user": "clojurewerkz/monger", "pwd": "monger", roles: ["dbAdmin"]})' monger-test4
