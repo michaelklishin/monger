993bb038d9bfa70587f75d63b86f17c0a19d42ce
冯忠孝 <32647908@qq.com>
2017-11-18 12:14:55 +0800

    ci test tip No command 'lein2' found, did you mean: test mongodb version up to 3.5

--- bin/ci/install_mongodb.sh
+++ bin/ci/install_mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org.list
+echo "deb http://repo.mongodb.org/apt/debian/dists/wheezy/mongodb-org/3.5/ main" | sudo tee /etc/apt/sources.list.d/mongodb-org.list
 
 sudo apt-get update
 sudo apt-get install -y mongodb-org
