a153eb8116050aa0cd1196e9771e3433c472539b
Michael Klishin <michael@clojurewerkz.org>
2018-12-02 21:38:56 +0300

    Travis: provision MongoDB 4.x

--- bin/ci/install_mongodb.sh
+++ bin/ci/install_mongodb.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 
 sudo apt-get update
 sudo apt-get install -y mongodb-org
